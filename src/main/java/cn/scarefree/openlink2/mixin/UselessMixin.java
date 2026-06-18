5b42e40950f0e983a39051e54258403ac445901b
Terry-MC123 <terry_mc@qq.com>
2026-06-18 11:24:03 +0800

    fix: stupid mixin problems, will be fixed when using mixins

---
+++ src/main/java/cn/scarefree/openlink2/mixin/UselessMixin.java
@@ -0,0 +1,9 @@
+package cn.scarefree.openlink2.mixin;
+
+import net.minecraft.client.gui.screens.TitleScreen;
+import org.spongepowered.asm.mixin.Mixin;
+
+//用来过编译
+@Mixin(TitleScreen.class)
+public class UselessMixin {
+}
